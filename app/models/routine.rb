f1c1ff811c21cd4b87b1e77db2da6ac0d0a890ed
Antony Parker <antonyqparker@gmail.com>
2016-02-22 14:11:31 +0000

    database ready

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,4 +1,4 @@
 class Routine < ActiveRecord::Base
-  belongs_to :users
+  belongs_to :user
   has_and_belongs_to_many :exercises
 end
--- db/migrate/20160222102544_create_user.rb
+++ db/migrate/20160222102544_create_user.rb
@@ -1,7 +1,7 @@
 class CreateUser < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :username
       t.string :age
       t.string :gender
       t.string :level
