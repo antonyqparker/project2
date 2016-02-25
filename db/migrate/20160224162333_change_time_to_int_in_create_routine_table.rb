6fa1bec2b8c328d12340ecfadb827f50f267f6c1
Antony Parker <antonyqparker@gmail.com>
2016-02-25 17:48:11 +0000

    Updated migration

--- db/migrate/20160224162333_change_time_to_int_in_create_routine_table.rb
+++ db/migrate/20160224162333_change_time_to_int_in_create_routine_table.rb
@@ -1,6 +1,6 @@
 class ChangeTimeToIntInCreateRoutineTable < ActiveRecord::Migration
   def change
     # ALTER TABLE create_routine ALTER duration TYPE integer USING duration::int;
-    rename_column :routines, :integer, :duration
+    rename_column :routines, :duration, :integer
   end
 end
